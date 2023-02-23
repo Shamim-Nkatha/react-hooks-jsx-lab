ed7a401b3a6bd423009c8aa01abfe21d41412881
Shamim-Nkatha <123588376+Shamim-Nkatha@users.noreply.github.com>
2023-02-23 21:02:16 +0300

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,10 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about"><h2>About Me</h2>
+            <p>A Master inn form #Guru Spikes</p>
+              <img src={image} alt="i made this"></img>
+            </div>;
 }
 
 export default About;
